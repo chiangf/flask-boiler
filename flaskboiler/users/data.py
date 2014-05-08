21e7ca90dc1ff684b77829705b07f43fed5e4667
Frank Chiang <fchiang@apperian.com>
2014-05-08 09:12:11 -0400

    Update data.py
    Add some docstring explaining the purpose of some_complicated_query()

--- flaskboiler/users/data.py
+++ flaskboiler/users/data.py
@@ -7,6 +7,9 @@ class UserData(Data):
         super(UserData, self).__init__(User)
 
     def some_complicated_query(self, username):
+        """
+        Not actually a complicated query but this shows how we might want to mix raw SQL with SQLAlchemy ORM.
+        """
         results = db.session.query(User).from_statement(
             """SELECT * FROM users WHERE username=:username"""
         ).params(username=username)
